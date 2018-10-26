2d7cc1db9582f6dc937862496968e082010a2e75
annacho <annacho13@gmail.com>
2018-10-26 16:45:49 -0700

    Add initial share place form

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -9,58 +9,14 @@ import {
   Button
 } from 'react-native';
 
-class PlaceInput extends Component {
-  state = {
-    placeName: "",
-    places: []
-  };
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  }
+const placeInput = props => (
+  <DefaultInput
+    placeholder="Place Name"
+    value={props.placeName}
+    onChangeText={props.onChangeText}
+  />
+);
 
-  placeAddedHandler = () => {
-    if (this.state.placeName.trim() === ""){
-      return;
-    }
-
-    this.props.onPlaceAdded(this.state.placeName);
-  };
-
-  render() {
-    return (
-      <View style={styles.inputContainer}
-        <TextInput
-          placeholder="An awesome place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-          style={styles.placeInput}
-          />
-          <Button
-            title="Add"
-            style={styles.placeButton}
-            onPress={this.placeSubmitHandler}
-          />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    // flex: 1,
-    width: "100%",
-    marginBottom: 5,
-    flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  },
-});
+export default placeInput;
