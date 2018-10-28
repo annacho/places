108ba1f4340167adf30cf51ee52a9734d5bb637e
annacho <annacho13@gmail.com>
2018-10-27 21:22:03 -0400

    Add styling and animations

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ImageBackground,
+  Dimensions
+} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
@@ -9,22 +17,75 @@ import ButtonWithBackground from '../../components/UI/ButtonWithBackground/Butto
 import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
+  state = {
+    viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", this.updateStyles);
+    });
+  }
+
+  componentWillUnmount(){
+    Dimensions.removeEventListener("change", this.updateStyles)
+  }
+
+  updateStyles = (dims) => {
+    this.setState({
+      viewMode:
+        dims.window.height > 500 ? "portrait" : "landscape"
+    });
+  }
+
   loginHandler = () => {
     startMainTabs();
   };
 
-  render () {
+  render() {
+    let headingText = null;
+
+    if (this.state.viewMode === "portrait") {
+      headingText = (
+        <MainText>
+          <HeadingText>Please Log In</HeadingText>
+        </MainText>
+      );
+    }
+
     return (
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.container}>
-          <MainText>
-            <HeadingText>Please Log In</HeadingText>
-          </MainText>
           <ButtonWithBackground color="#29aaf4" onPress={() => alert("Hello")}>Switch to Login</ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="Your E-Mail Address" style={styles.input} />
-            <DefaultInput placeholder="Password" style={styles.input} />
-            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+            <View
+              style={
+                this.state.viewMode === "portrait"
+                  ? styles.portraitPasswordContainer
+                  : styles.landscapePasswordContainer
+              }
+            >
+              <View
+                style={
+                  this.state.viewMode === "portrait"
+                    ? styles.portraitPasswordWrapper
+                    : styles.landscapePasswordWrapper
+                }
+              >
+                <DefaultInput placeholder="Password" style={styles.input} />
+              </View>
+              <View
+                style={
+                  this.state.viewMode === "portrait"
+                    ? styles.portraitPasswordWrapper
+                    : styles.landscapePasswordWrapper
+                }
+              >
+                <DefaultInput placeholder="Confirm Password" style={styles.input} />
+              </View>
+            </View>
           </View>
           <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithBackground>
         </View>
@@ -49,6 +110,20 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: "#eee",
     borderColor: "#bbb"
+  },
+  landscapePasswordContainer: {
+    flexDirection: "row"
+    justifyContent: "space-between"
+  },
+  portraitPasswordContainer: {
+    flexDirection: "column"
+    justifyContent: "flex-start"
+  },
+  landscapePasswordWrapper: {
+    width: "45%"
+  },
+  portraitPasswordWrapper: {
+    width: "100%"
   }
 })
 
--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -8,7 +8,9 @@ import {
   Image,
   Button,
   Modal,
-  TouchableOpacity
+  TouchableOpacity,
+  Platform,
+  Dimensions
 } from 'react-native';
 import { connect } from 'react-redux';
 
@@ -16,6 +18,24 @@ import Icon from "react-native-vector-icons/Ionicons";
 import { deletePlace } from '../../store/actions/index';
 
 class PlaceDetail extends Component {
+  state = {
+    viewMode: "portrait"
+  }
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", this.updateStyles)
+  };
+
+  componentWillUnmount(){
+    Dimensions.removeEventListener("change", this.updateStyles);
+  }
+
+  updateStyles = dims => {
+    this.setState({
+      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+    })
+  };
 
   placeDeletedHandler = () => {
     this.props.onDeletePlace(this.props.selectedPlace.key);
@@ -24,17 +44,32 @@ class PlaceDetail extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View>
+      <View
+        style={[
+          styles.container,
+          this.state.viewMode === "portrait"
+          ? styles.portraitContainer
+          : styles.landscapeContainer
+        ]}
+      >
+        <View style={styles.subContainer}>
           <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
-          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
         </View>
-        <View>
-          <TouchableOpacity onPress={this.placeDeletedHandler}>
-            <View style={styles.deleteButton}>
-              <Icon size={30} name="ios-trash" color="red" />
-            </View>
-          </TouchableOpacity>
+        <View style={styles.subContainer}>
+          <View>
+            <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+          </View>
+          <View>
+            <TouchableOpacity onPress={this.placeDeletedHandler}>
+              <View style={styles.deleteButton}>
+                <Icon
+                  size={30}
+                  name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
+                  color="red"
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
@@ -44,6 +79,13 @@ class PlaceDetail extends Component {
 const styles = StyleSheet.create({
   container: {
     margin: 22,
+    flex: 1
+  },
+  portraitContainer: {
+    flexDirection: "column"
+  },
+  landscapeContainer: {
+    flexDirection: "row"
   },
   placeImage: {
     width: "100%",
@@ -53,6 +95,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  subContainer: {
+    flex: 1
   }
 });
 
