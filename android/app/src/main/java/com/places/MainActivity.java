06abc9454a9078c4f73976d85c4c00ee811befb9
annacho <annacho13@gmail.com>
2018-11-06 19:57:37 -0800

    Publish app

--- android/app/src/main/java/com/places/MainActivity.java
+++ android/app/src/main/java/com/places/MainActivity.java
@@ -1,6 +1,11 @@
 package com.places;
 
 import com.facebook.react.ReactActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,22 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "rncourse";
+    }
+
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        TextView textview = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#521751"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#fa923f"));
+        textView.setText("Awesome Places");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
     }
 }
